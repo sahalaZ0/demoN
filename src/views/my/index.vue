0cedad5bcc4b8c9d2e50bb56fae5660588e1561f
‘sahalaZ0’ <254274265@qq.com>
2022-07-01 09:51:56 +0800

    头条项目搜索提交

---
+++ src/views/my/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="my-container">
+    <div class="header">
+      <img
+        class="mobile-img"
+        src="~@/assets/mobile.png"
+        @click="$router.push('/login')"
+      >
+    </div>
+    <div class="grid-nav"></div>
+    <van-cell title="消息通知" is-link url="" />
+    <van-cell title="实名认证" is-link url="" />
+    <van-cell title="用户反馈" is-link url="" />
+    <van-cell title="小智同学" is-link url="" />
+    <van-cell title="系统设置" is-link url="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyIndex',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less">
+.my-container {
+  > .header {
+    height: 361px;
+    background: url("~@/assets/banner.png") no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .mobile-img {
+      width: 132px;
+      height: 132px;
+    }
+  }
+}
+</style>
